99afd5a3d9df99044dcb87b43ae46f8746d1abb6
Abraham Navarro <abrahancho6@gmail.com>
2023-07-12 06:10:27 -0400

    refactor

--- cypress/e2e/upload_a_image.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Upload a image", () => {
-  it("upload a image using button", () => {
-    cy.visit("http://localhost:3000");
-
-    //cy.get('[data-testid="upload-button"]').selectFile("public/devchallenges.png");
-
-    cy.contains('Uploaded Succesfully!');
-  });
-});
